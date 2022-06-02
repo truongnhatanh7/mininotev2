759f9a1280b5425c3360167127bb3025a02972bc
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-06-02 15:29:48 +0700

    Initial Commit

---
+++ mininotev2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mininotev2
+//
+//  Created by Truong Nhat Anh on 02/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mininotev2/mininotev2App.swift
@@ -0,0 +1,17 @@
+//
+//  mininotev2App.swift
+//  mininotev2
+//
+//  Created by Truong Nhat Anh on 02/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct mininotev2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
